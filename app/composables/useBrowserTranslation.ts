fccf28d4d664b36eac03fe411fa889ec65f2f115
hengxuandong <hengxuan.dong@gmail.com>
2026-04-01 22:14:02 -0700

    Fix comment to clarify language translation handling for Chinese version

--- app/composables/useBrowserTranslation.ts
+++ app/composables/useBrowserTranslation.ts
@@ -1,8 +1,8 @@
 /**
  * useBrowserTranslation - Sets up browser translation trigger for non-English languages
  *
- * When a user is viewing the Chinese version (/zh/), we set the document's lang attribute
- * to 'zh-Hans' to signal to the browser that automatic translation might be helpful,
+ * When a user is viewing the Chinese translation (?translation=zh-Hans), we set the document's
+ * lang attribute to 'zh-Hans' to signal to the browser that automatic translation might be helpful,
  * even though the actual content is in English.
  */
 
